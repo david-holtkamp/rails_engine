425b59596206141c4d76663b5d972c35f57c95f6
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-19 15:47:12 -0600

    change before action to validation

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,6 @@ require_relative 'modules/priceable'
 class Item < ApplicationRecord
   include Priceable
 
-  before_create :convert_to_dollars
-  belongs_to :merchant 
+  before_validation :convert_to_dollars
+  belongs_to :merchant
 end
