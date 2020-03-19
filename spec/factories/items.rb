e6cc5d893378d6c51f805081e655f986ae1a923e
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-19 16:41:35 -0600

    Item create spec and implementation

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { "Useful Item" }
     description { "Apocalypse prep tools." }
-    unit_price { 123.45 }
+    unit_price { "12345" }
     created_at { "2012-03-27 14:53:59 UTC" }
     updated_at { "2012-03-27 14:53:59 UTC" }
     merchant
