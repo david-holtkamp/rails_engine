861f8a36547e17dc7ea06123de8b03bb97abdf03
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-20 15:54:29 -0600

    Routing for items finder actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/items', to: 'items#index'
-      get '/items/:id', to: 'items#show'
-      post '/items', to: 'items#create'
-
       namespace :items do
         get '/:id/merchant', to: 'merchants#show'
+        get '/find', to: 'finders#show'
+        get '/find_all', to: 'finders#index'
       end
 
       get '/merchants', to: 'merchants#index'
@@ -15,6 +13,10 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/:id/items', to: 'items#index'
       end
+
+      get '/items', to: 'items#index'
+      get '/items/:id', to: 'items#show'
+      post '/items', to: 'items#create'
     end
   end
 end
