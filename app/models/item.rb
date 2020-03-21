7246847d06f5e912dc5d3c270e5d8eee88e208cc
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-21 14:59:14 -0600

    Comment out module method and validate presence of attributes

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ require_relative 'modules/priceable'
 class Item < ApplicationRecord
   include Priceable
 
-  before_validation :convert_to_dollars
+  # before_validation :convert_to_dollars
   belongs_to :merchant
+  validates_presence_of :name, :description, :unit_price, :merchant_id
 end
