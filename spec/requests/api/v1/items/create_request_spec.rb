f8a62fef45eec437311d872fb7ffad6b3776897b
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-21 14:58:25 -0600

    Change create item test to be more thorough

--- spec/requests/api/v1/items/create_request_spec.rb
+++ spec/requests/api/v1/items/create_request_spec.rb
@@ -4,14 +4,16 @@ RSpec.describe 'Items API' do
   it "can create a new item" do
     merchant = create(:merchant)
 
-    item_params = { name: "Saw",
+    post "/api/v1/items", params: {name: "Saw",
                     description: "I want to play a game",
-                    unit_price: 87.50,
-                    merchant_id: merchant.id}
+                    unit_price: "87.50",
+                    merchant_id: "#{merchant.id}"}
 
-    post "/api/v1/items", params: {item: item_params}
-    item = Item.last
     expect(response).to be_successful
-    expect(item.name).to eq(item_params[:name])
+    json_item = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+    item = Item.last
+
+    expect(item.name).to eq(json_item[:name])
   end
 end
