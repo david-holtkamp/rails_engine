be46699c653a0f1ff59ead678a02532dbdc8b396
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-22 12:27:10 -0600

    Establish belongs to relationship for invoices items

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -3,5 +3,8 @@ require_relative 'modules/priceable'
 class InvoiceItem < ApplicationRecord
   include Priceable
 
+  belongs_to :item
+  belongs_to :invoice
+
   # before_validation :convert_to_dollars
 end
