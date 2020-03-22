c7d488f4fd5d53925eb423bdc72da3cea9eae5a9
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-22 12:25:23 -0600

    Testing to destroy an item

--- spec/requests/api/v1/items/destroy_spec.rb
+++ spec/requests/api/v1/items/destroy_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe 'Items API' do
   it "can destroy an item" do
-  item = create(:item)
+    item = create(:item)
 
-  expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
+    expect{ delete "/api/v1/items/#{item.id}" }.to change(Item, :count).by(-1)
 
-  expect(response).to be_success
-  expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
-end
+    expect(response).to be_successful
+    expect{Item.find(item.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
