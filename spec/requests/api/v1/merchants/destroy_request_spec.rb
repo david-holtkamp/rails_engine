80881705c247cd9d88f6bea168820135f290b8f9
David Holtkamp <david.hholtkamp@gmail.com>
2020-03-22 23:06:53 -0600

    Test for destroying a merchant

---
+++ spec/requests/api/v1/merchants/destroy_request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchants API' do
+  it "can destroy an merchant" do
+    merchant = create(:merchant)
+
+    expect{ delete "/api/v1/merchants/#{merchant.id}" }.to change(Merchant, :count).by(-1)
+
+    expect(response).to be_successful
+    expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+end
